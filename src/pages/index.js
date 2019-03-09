bba71c9edfc04165d22317c9d179ec677a1049d5
Jesse Duffield <jessedduffield@gmail.com>
2019-03-09 15:02:42 +1100

    playing around

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,9 @@
-import React from "react"
+import React from 'react'
 
-export default () => <div>Hello world!</div>
+import { Link } from 'gatsby'
+
+export default () => (
+  <div>
+    <Link to="/about/">Contact</Link>Hello world2!
+  </div>
+)
